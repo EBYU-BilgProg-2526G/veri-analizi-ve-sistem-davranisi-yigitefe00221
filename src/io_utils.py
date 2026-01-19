7bc550479611796bc6ef2e6d52dca541a94790db
yigitefe00221 <yigitefegokyildiz@gmail.com>
2026-01-19 13:46:47 +0300

    Update io_utils.py

--- src/io_utils.py
+++ src/io_utils.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-CSV dosyasından sinyal verisi okuma yardımcı fonksiyonları
-"""
+
+import csv
+import numpy as np
 
 def load_signal_csv(path):
-    """
     CSV formatı:
     t,x
     0.0, 1.23
     0.01, 1.10
-    ...
+    
+    t = []
+    x = []
 
-    Parametre:
-        path (str): CSV dosya yolu
+    with open(path, "r") as file:
+        reader = csv.reader(file)
+        next(reader)  # başlık satırını atla (t,x)
 
-    Dönen:
-        t : zaman vektörü
-        x : sinyal vektörü
-    """
-    # TODO:
-    # 1. csv modülünü kullan
-    # 2. dosyayı satır satır oku
-    # 3. t ve x listelerini doldur
-    # 4. numpy array olarak döndür
-    pass
+        for row in reader:
+            t.append(float(row[0]))
+            x.append(float(row[1]))
+
+    t = np.array(t)
+    x = np.array(x)
+
+    return t, x
